f2849ec4cbda0d6a7b35058478bc29d88ddc6324
Saumya.Prabhakar <saumya.prabhakar@bhavnacorp.com>
2022-11-28 16:51:07 +0530

    Commited Changes

--- src/com/bhavna/services/ZipServicesV2.java
+++ src/com/bhavna/services/ZipServicesV2.java
@@ -1,26 +1,26 @@
 package com.bhavna.services;
 
-import com.bhavna.model.Address;
+import com.bhavna.model.Area;
 import com.bhavna.services.ZipServicesV1;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ZipServicesV2 extends ZipServicesV1 {
-    List<Address> list = new ArrayList<>();
+    List<Area> list = new ArrayList<>();
 
     public void searchAddress(long code) {
-        Address data1 = new Address("India", "Maharashtra", "Pune", 200011);
-        Address data2 = new Address("India", "Uttarpradesh", "Ghaziabad", 200014);
-        Address data3 = new Address("India", "Bihar", "Patna", 200012);
-        Address data4 = new Address("India", "Assam", "Guhati", 200013);
-        Address data5 = new Address("India", "Himachal Pradesh", "Baddi", 210302);
-        Address data6 = new Address("India", "Punjab", "Mohali", 201303);
-        Address data7 = new Address("India", "Rajasthan", "Jaisalmer", 201304);
-        Address data8 = new Address("UK", "London", "WestMinster", 101304);
-        Address data9 = new Address("UK", "London", "Buckingham", 101305);
-        Address data10 = new Address("USA", "California", "Sacramento", 101306);
-        Address data11 = new Address("USA", "Texas", "Austin", 101307);
+        Area data1 = new Area("India", "Maharashtra", "Pune", 200011);
+        Area data2 = new Area("India", "Uttarpradesh", "Ghaziabad", 200014);
+        Area data3 = new Area("India", "Bihar", "Patna", 200012);
+        Area data4 = new Area("India", "Assam", "Guhati", 200013);
+        Area data5 = new Area("India", "Himachal Pradesh", "Baddi", 210302);
+        Area data6 = new Area("India", "Punjab", "Mohali", 201303);
+        Area data7 = new Area("India", "Rajasthan", "Jaisalmer", 201304);
+        Area data8 = new Area("UK", "London", "WestMinster", 101304);
+        Area data9 = new Area("UK", "London", "Buckingham", 101305);
+        Area data10 = new Area("USA", "California", "Sacramento", 101306);
+        Area data11 = new Area("USA", "Texas", "Austin", 101307);
 
 
         list.add(data1);
@@ -36,7 +36,7 @@ public class ZipServicesV2 extends ZipServicesV1 {
         list.add(data11);
 
         boolean flag = false;
-        for (Address address : list) {
+        for (Area address : list) {
 
             if (address.getZipCode() == code) {
                 System.out.println(address);
@@ -56,7 +56,7 @@ public class ZipServicesV2 extends ZipServicesV1 {
     }
 
     public void allRecord(String records) {
-        for (Address record : list) {
+        for (Area record : list) {
             if (records.equals("yes"))
 
                 System.out.println(record);
